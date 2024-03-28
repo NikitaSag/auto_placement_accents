107c163827972e07083fa67b6c93bdcd5c3cbafb
NikitaSag <sagociyan5411@gmail.com>
2024-03-28 10:23:17 +0300

    new project

--- src/options/config_read.py
+++ src/options/config_read.py
@@ -5,6 +5,6 @@ import os.path
 
 # Функция для чтения конфигурационного файла
 def get_config_data():
-    with open('C:\\Users\\Zak\\auto_placement_accents\\src\\options\\config.yaml', 'r') as file_option:
+    with open('C:\\Users\\Zak\\PycharmProjects\\auto_placement_accents\\src\\options\\config.yaml', 'r') as file_option:
         options = yaml.safe_load(file_option)
     return options
--- src_v2/option_v2/config_read_v2.py
+++ src_v2/option_v2/config_read_v2.py
@@ -5,6 +5,7 @@ import os.path
 
 # Функция для чтения конфигурационного файла
 def get_config_data():
-    with open('C:/Users/Zak/auto_placement_accents/src_v2/option_v2/config_v2.yaml', 'r') as file_option:
+    with open('C:\\Users\\Zak\\PycharmProjects\\auto_placement_accents\\src_v2\\option_v2\\config_v2.yaml',
+              'r') as file_option:
         options = yaml.safe_load(file_option)
     return options
